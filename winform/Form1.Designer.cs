51b997136adde6b6a69b20afeb5ecea383407f9f
Thang Phan <phanthq2k2@gmail.com>
2024-03-13 09:06:48 +0700

    update label

--- winform/Form1.Designer.cs
+++ winform/Form1.Designer.cs
@@ -37,41 +37,41 @@
             // lbusername
             // 
             lbusername.AutoSize = true;
-            lbusername.Location = new Point(55, 22);
+            lbusername.Location = new Point(95, 44);
             lbusername.Name = "lbusername";
-            lbusername.Size = new Size(73, 20);
+            lbusername.Size = new Size(109, 20);
             lbusername.TabIndex = 0;
-            lbusername.Text = "username";
+            lbusername.Text = "Tên Đăng nhập";
             // 
             // txtusername
             // 
-            txtusername.Location = new Point(161, 22);
+            txtusername.Location = new Point(210, 41);
             txtusername.Name = "txtusername";
-            txtusername.Size = new Size(157, 27);
+            txtusername.Size = new Size(157, 26);
             txtusername.TabIndex = 1;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(161, 104);
+            txtPassword.Location = new Point(210, 119);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(157, 27);
+            txtPassword.Size = new Size(157, 26);
             txtPassword.TabIndex = 3;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(55, 104);
+            label1.Location = new Point(134, 119);
             label1.Name = "label1";
-            label1.Size = new Size(73, 20);
+            label1.Size = new Size(70, 20);
             label1.TabIndex = 2;
-            label1.Text = "Password:";
+            label1.Text = "Mật khẩu";
             label1.Click += label1_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(8F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 451);
+            ClientSize = new Size(531, 261);
             Controls.Add(txtPassword);
             Controls.Add(label1);
             Controls.Add(txtusername);
